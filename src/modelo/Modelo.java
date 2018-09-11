e180c45e2c084a018a3562e5742c3e434ef10a86
1107092701 <1107092701@SC202E12.academico.icesi.edu.co>
2018-09-11 12:41:26 -0500

    Prueba de que esta funcionando

---
+++ src/modelo/Modelo.java
@@ -0,0 +1,41 @@
+package modelo;
+
+import java.util.ArrayList;
+
+public class Modelo{
+	
+
+	
+	public static String [][] meallyReduccion (String [][] maquina) {
+		String [][] respuesta = new String [maquina.length][maquina[0].length];
+		
+		//en esta parte se hara la primera paricion de dos elementos
+		String [] primerParticion =  new String [2];
+		for (int i = 0; i < respuesta.length; i++) {
+			//en el caso que la salida sea cero
+			if (respuesta[maquina.length-1][i].equals("0")) {
+				
+			}
+			//cuando la salida es 1
+			else {
+				
+			}
+			
+			
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		return respuesta;
+	}
+	
+	
+	
+	
+
+}
