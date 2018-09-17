15d38ed7459011e5d3672d39329d7a482943fc19
Eileen Guerrero <eileenguerrerogomez@gmail.com>
2018-09-16 22:29:42 -0500

    Metodo verificarParticion() y buscar() ya se supone que estan listos.

--- src/modelo/Modelo.java
+++ src/modelo/Modelo.java
@@ -25,10 +25,11 @@ public class Modelo{
 			if (cantidadConectados==0) {
 				maquina[i][maquina[0].length-1]="2";
 			}
-			
+			//reinicio el contador
+			cantidadConectados=0;
 		}
 		
-		//en esta parte se hara la primera paricion de dos elementos
+		//en esta parte se hara la primera paricion de dos elementos. Se hacen dos grupos. Los de output 1 y 0.
 		ArrayList<String> primerParticion =  new ArrayList<String>(2);
 		for (int i = 1; i < maquina.length; i++) {
 			//en el caso que la salida sea cero
@@ -40,10 +41,26 @@ public class Modelo{
 				primerParticion.add(1,maquina[maquina.length-1][i]+",");
 			}	
 		}
-		//ahora vamos a hacer los siguientes pariticonamientos a partir de los dos pariticionamientos creados anteriormente.
+		/**
+		 * Ahora vamos a hacer las demas particiones. Buscamos de a parejas y cada pareja se verifica
+		 * que pertenezca a la particion establecida anteriormente. 
+		 * 
+		 */
 		
+		//saco el lenguaje aceptado para buscar y recorrer
+		ArrayList<String> lenguajeAceptado = new ArrayList<String>();
+		ArrayList<String> partiActual = primerParticion;
+		for (int i = 1; i < maquina.length-1; i++) {
+			lenguajeAceptado.add(maquina[i][0]);
+		}
+		for (int i = 2; i < lenguajeAceptado.size(); i=i+2) {
+			boolean particionExiste=verificarParticion(maquina,lenguajeAceptado.get(i-2),lenguajeAceptado.get(i-1), partiActual);
+			//si la particion no existe
+			if (!particionExiste) {
+				
+			}
+		}
 		
-		//hacemos la busqueda de cada pareja de elementos para verificar si pertenecen a cada particion
 		
 		
 		//renombramiento
@@ -57,28 +74,31 @@ public class Modelo{
 		
 		
 		
-		return respuesta;
+		return null;
 	}
 	
 	/**
-	 * Este metodo verifica que la particion existe en la particion anterior.
+	 * Este metodo verifica que la particion existe en la particion anterior. Es decir, si quiero ver si a y b pertenecen
+	 * a la particion, este algoritmo me dice true o false.
 	 * @param maquina Maquina de Meally
 	 * @param a 
 	 * @param b 
 	 * @return true si la particion esta dentro de la particion anterior.
 	 */
-	public boolean verificarParticion (String [][] maquina, String a, String b, String [] particion) {
+	public static boolean verificarParticion (String [][] maquina, String a, String b, ArrayList<String> particionAnterior) {
 		ArrayList<String> fila1 = buscarValoresMaquina(a, maquina);
 		ArrayList<String> fila2 = buscarValoresMaquina(b, maquina);
+		boolean respuesta = false;
 		if ( (fila1.size()>0) && (fila2.size()>0) ) {
 			for (int i = 0; i < fila1.size(); i++) {
-				// buscamos si cada pareja pertenece al particionamiento anterior
+				for (int j = 0; j < particionAnterior.size(); j++) {
+					if(particionAnterior.get(j).contains(fila1.get(i)) || particionAnterior.get(j).contains(fila2.get(i)) ){
+						respuesta=true;
+					}
+				}
 			}
 		}
-		
-		
-		
-		return true;
+		return respuesta;
 	}
 	
 
@@ -88,7 +108,7 @@ public class Modelo{
 	 * @param maquina
 	 * @return
 	 */
-	public ArrayList<String> buscarValoresMaquina (String a, String [][] maquina) {
+	public static ArrayList<String> buscarValoresMaquina (String a, String [][] maquina) {
 		ArrayList<String> respuesta = new ArrayList<String>();
 		for (int i = 1; i < maquina.length; i++) {
 			
