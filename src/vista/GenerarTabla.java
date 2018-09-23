b07718fedc4feceab4d59b1fa6efe386db07f46f
sarai romero <sarairomero1025@hotmail.com>
2018-09-23 16:10:26 -0500

    clase principal y modificaciones en generar tabla...

--- src/vista/GenerarTabla.java
+++ src/vista/GenerarTabla.java
@@ -8,20 +8,20 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 @SuppressWarnings("serial")
-public class GenerarTabla extends JFrame {
+public class GenerarTabla extends JPanel {
 
 	private JTextField estados;
 	private JTextField largo_vocabulario;
 	
 	public GenerarTabla() {
-		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		//this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setLayout(null);
-		this.setResizable(false);
-		this.setPreferredSize(new Dimension(300, 200));
-		
+		//this.setResizable(false);
+		this.setPreferredSize(new Dimension(250, 240));
 		// Agregar campo de numero de estados
 		JLabel est = new JLabel("Número estados:");
 		est.setBounds(10, 10, 150, 30);
@@ -31,7 +31,7 @@ public class GenerarTabla extends JFrame {
 		this.add(estados);
 		
 		// Agregar campo de tamanio vocabulario
-		JLabel voc = new JLabel("Largo Alfabeto:");
+		JLabel voc = new JLabel("Número entradas:");
 		voc.setBounds(10, 40, 150, 30);
 		this.add(voc);
 		largo_vocabulario= new JTextField();
@@ -39,8 +39,8 @@ public class GenerarTabla extends JFrame {
 		this.add(largo_vocabulario);
 		
 		// Agregar boton para generar
-		JButton buton = new JButton("Generar");
-		buton.setBounds(100, 120, 100, 30);
+		JButton buton = new JButton("Generar Automata Moore");
+		buton.setBounds(10, 120, 200, 30);
 		buton.addActionListener(new ActionListener() {
 			
 			@Override
@@ -57,14 +57,32 @@ public class GenerarTabla extends JFrame {
 				}
 			}
 		});
+		
+		// Agregar boton para generar automata de mealy
+		JButton butonMealy = new JButton("Generar Automata Mealy");
+		butonMealy.setBounds(10, 160, 200, 30);
+		butonMealy.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				int esta = 0;
+				int alfa = 0;
+				try {
+					esta = Integer.parseInt(estados.getText());
+					alfa = Integer.parseInt(largo_vocabulario.getText());
+					TablaMealy t = new TablaMealy(esta,alfa);
+					t.setVisible(true);
+				} catch (Exception exception){
+					JOptionPane.showMessageDialog(null, "El numero de estado u el largo de el alfabeto deben ser numeros enteros");
+				}
+			}
+		});
+
+
 		this.add(buton);
-		this.pack();
+		this.add(butonMealy);
+		//this.pack();
 	}
 	
-	
-	
-	public static void main(String[] args) {
-		GenerarTabla v = new GenerarTabla();
-		v.setVisible(true);
-	}
+
 }
